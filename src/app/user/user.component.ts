e2321d7472072f4fce5093b5c4c11abf30cc5e6a
Praveen Thirumalai <gtpraveen01@gmail.com>
2025-11-10 20:56:58 -0500

    Initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component,Input, input} from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+// const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+
+
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true}) avatar!: string ;
+  @Input({ required:true}) name!: string ;
+
+ get imagePath() { 
+   return 'assets/users/' + this.avatar;
+ }
+
+  onSelectUser() {   }
+
+}
